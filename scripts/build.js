49edb4f80913deaea6abfb146814bcd233054f1f
Frederic Hemberger <mail@frederic-hemberger.de>
2016-10-22 20:20:18 +0200

    Split build/serve script

---
+++ scripts/build.js
@@ -0,0 +1,106 @@
+#!/usr/bin/env node
+'use strict';
+
+const Path = require('path');
+const Metalsmith = require('metalsmith');
+
+const autoprefixer = require('autoprefixer-stylus');
+const collections = require('metalsmith-collections');
+const feed = require('metalsmith-feed');
+const layouts = require('metalsmith-layouts');
+const markdown = require('metalsmith-markdown');
+const prism = require('metalsmith-prism');
+const permalinks = require('metalsmith-permalinks');
+const stylus = require('metalsmith-stylus');
+
+// Custome metalsmith plugins
+const externalCollections = require('./plugins/external-collections.js');
+const mergeCollections = require('./plugins/merge-collections.js');
+const filterStylusPartials = require('./plugins/filter-stylus-partials.js');
+const cssshrink = require('./plugins/cssshrink.js');
+
+// Handlebars helpers
+const startswith = require('./helper/startswith.js');
+const strftime = require('./helper/strftime.js');
+const equals = require('./helper/equals.js');
+
+
+function build () {
+
+    console.time('[metalsmith] Build finished');
+    Metalsmith(process.cwd())
+        .metadata({
+            site: {
+                title  : 'Frederic Hemberger',
+                author : 'Frederic Hemberger',
+                url    : 'https://frederic-hemberger.de'
+            }
+        })
+        .use(collections({
+            artikel  : { pattern: 'artikel/*.md', refer: false },
+            articles : { pattern: 'articles/*.md', refer: false },
+            thoughts : { pattern: 'thoughts/*.md', refer: false }
+        }))
+        .use(markdown({ langPrefix: 'language-' }))
+        .use(prism())
+        .use(filterStylusPartials())
+        .use(stylus({
+            compress: true,
+            use: [autoprefixer()]
+        }))
+        .use(cssshrink([
+            'static/css/styles.css',
+            'static/css/article.css'
+        ]))
+        .use(permalinks({
+            pattern: ':collection/:title',
+            relative: false,
+            slug: {
+                lower: true,
+                charmap: {
+                    'Ä': 'ae', 'Ö': 'oe', 'Ü': 'ue',
+                    'ä': 'ae', 'ö': 'oe', 'ü': 'ue', 'ẞ': 'ss'
+                }
+            }
+        }))
+        .use(externalCollections({
+            externalArticles : `${process.cwd()}/data/external_articles.yaml`,
+            podcasts         : `${process.cwd()}/data/podcasts.yaml`,
+            talks            : `${process.cwd()}/data/talks.yaml`
+        }))
+        .use(mergeCollections(['artikel', 'articles', 'externalArticles'], 'articles'))
+        .use(mergeCollections(['artikel', 'articles', 'podcasts', 'talks'], 'all'))
+        .use(feed({
+            collection: 'talks',
+            destination: 'feeds/talks.rss'
+        }))
+        .use(feed({
+            collection: 'all',
+            destination: 'feeds/feed.rss'
+        }))
+        // .use(feed({
+        //   collection: 'thoughts',
+        //   destination: 'feeds/thoughts.rss'
+        // }))
+        .use(layouts({
+            engine: 'handlebars',
+            partials: 'layouts/partials',
+            helpers: {
+                startswith,
+                strftime,
+                equals
+            }
+        }))
+        .build((err) => {
+
+            if (err) { throw err; }
+            console.timeEnd('[metalsmith] Build finished');
+        });
+}
+
+
+if (!module.parent) {
+    return build();
+}
+
+module.exports = build;
---
+++ scripts/serve.js
@@ -0,0 +1,54 @@
+#!/usr/bin/env node
+'use strict';
+
+const Path = require('path');
+const Http = require('http');
+const st = require('st');
+const Chokidar = require('chokidar');
+
+const build = require('./build.js');
+
+
+function serve () {
+
+    const mount = st({
+        path: Path.join(process.cwd(), 'build'),
+        cache: false,
+        index: 'index.html'
+    });
+
+    Http
+        .createServer((req, res) => mount(req, res))
+        .listen(8080, () => console.log('[metalsmith] Server runs at http://localhost:8080/'));
+
+    /** File Watches for Re-Builds **/
+    const opts = {
+        persistent             : true,
+        ignoreInitial          : true,
+        followSymlinks         : true,
+        usePolling             : true,
+        alwaysStat             : false,
+        depth                  : undefined,
+        interval               : 100,
+        ignorePermissionErrors : false,
+        atomic                 : true
+    };
+
+    const change = (path) => {
+
+        const relativePath = path.replace(`${process.cwd()}/`, '');
+        console.log(`[metalsmith] File ${relativePath} was changed. Rebuilding ...`);
+        build();
+    };
+
+    ['src', 'layouts', 'data']
+        .map((dir) => Chokidar.watch(Path.join(process.cwd(), dir), opts))
+        .forEach((watched) => {
+            watched.on('change', change);
+            watched.on('add', change);
+        });
+}
+
+
+build();
+serve();

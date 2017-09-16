bb5f94bfe44a225644090039187fa24468083c06
Frederic Hemberger <mail@frederic-hemberger.de>
2017-09-16 07:13:15 +0200

    add-subtree: Check if subtree already exists

--- scripts/add-subtree.sh
+++ scripts/add-subtree.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
-git remote add talks https://github.com/fhemberger/talks.git || true
-git subtree add --squash --prefix=talks/ talks gh-pages || true
+if [[ `git remote | grep talks | wc -l` -eq 0 ]]; then
+    git remote add talks https://github.com/fhemberger/talks.git || true
+    git subtree add --squash --prefix=talks/ talks gh-pages || true
+fi

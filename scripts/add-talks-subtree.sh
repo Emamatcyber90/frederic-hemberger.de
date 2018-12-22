87f31882608aef547b7b7bf79587079208501ccc
Frederic Hemberger <mail@frederic-hemberger.de>
2018-12-22 15:02:55 +0100

    fix: shellcheck add-talks-subtree.sh

--- scripts/add-talks-subtree.sh
+++ scripts/add-talks-subtree.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [[ `git remote | grep talks | wc -l` -eq 0 ]]; then
-    git remote add talks https://github.com/fhemberger/talks.git || true
-    git subtree add --squash --prefix=talks/ talks gh-pages || true
+if [ "$(git remote | grep -c talks)" -eq 0 ]; then
+  git remote add talks https://github.com/fhemberger/talks.git || true
+  git subtree add --squash --prefix=talks/ talks gh-pages || true
 fi
